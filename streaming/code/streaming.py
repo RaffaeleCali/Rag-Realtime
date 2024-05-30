78cf6ff2eb7fab3b8b14a6da58c108d77c46611e
RaffaeleCali <raffalo8888@gmail.com>
2024-05-30 14:40:55 +0200

    configurazione spark

--- streaming/code/streaming.py
+++ streaming/code/streaming.py
@@ -5,20 +5,32 @@ from pyspark.sql import types as st
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.functions import create_map, lit
 from pyspark.sql.types import StructType, StructField, StringType
+import sys 
+from elasticsearch import Elasticsearch
 
+import hashlib
+def generate_sha256_hash_from_text(text) -> str:
+    # Create a SHA256 hash object
+    sha256_hash = hashlib.sha256()
+    # Update the hash object with the text encoded to bytes
+    sha256_hash.update(text.encode('utf-8'))
+    # Return the hexadecimal representation of the hash
+    return sha256_hash.hexdigest()
 spark = SparkSession.builder.appName("kafkatospark").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 kafkaServer="broker:9092"
 topic = "datapipe"
+es_host = "http://elasticsearch:9200"
+es_index = "spark-index"
+es = Elasticsearch(
+    es_host,
+)
+#from sentence_transformers import SentenceTransformer
 
-
-import chromadb
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("nomic-ai/nomic-embed-text-v1", trust_remote_code=True)
+#model = SentenceTransformer("nomic-ai/nomic-embed-text-v1", trust_remote_code=True)
 
 class CustomEmbeddingFunction:
-    def __init__(self, ):
+    def __init__(self,model ):
         self.model = model
 
     def __call__(self, input):
@@ -35,11 +47,8 @@ class CustomEmbeddingFunction:
             return []
 
 # Creazione dell'istanza della classe di funzione di embedding
-embedding_function = CustomEmbeddingFunction()
+#embedding_function = CustomEmbeddingFunction(model)
 
-# Utilizzo dell'istanza per aggiungere testi
-client = chromadb.PersistentClient()
-collection = client.get_or_create_collection(name="test", embedding_function=embedding_function)
 
 article_schema = StructType([
         StructField("url", StringType(), True),
@@ -70,11 +79,29 @@ df = spark.readStream.format('kafka') \
         .option("startingOffsets", "latest") \
         .load() \
         .select(from_json(col("value").cast("string"), schema).alias("data")) \
-        .selectExpr("data.*")
+        .selectExpr("data.articles.content")
 #.select(from_json(df.json, schema).alias('rowdata')) \
         
+def process_batch(batch_df, batch_id):
+    for row in batch_df.collect():
+        document = row['content']  
+        hash = generate_sha256_hash_from_text(document)
+        try:
+            
+            resp = es.index(index=es_index, id=hash, document=document)
+            print(resp)
+        except Exception as e:
+            print(f"\rErrore nell'invio del documento a Elastic: {e}",end = "")
+
+
+
 
 df.writeStream \
-.format("console") \
-.start() \
-.awaitTermination()
+    .foreachBatch(process_batch) \
+    .start() \
+    .awaitTermination()
+
+#df.writeStream \
+#.format("console") \
+#.start() \
+#.awaitTermination()
