ac5b617dc1e7e7ac33fdede27d92225744a9dc70
RaffaeleCali <raffalo8888@gmail.com>
2024-06-01 02:37:36 +0200

    cosi funziona tutto (da ripulire la carte volume di elastic)

--- streaming/code/streaming.py
+++ streaming/code/streaming.py
@@ -1,4 +1,4 @@
-from pyspark.ml import PipelineModel
+
 from pyspark.sql import SparkSession
 from pyspark.streaming import StreamingContext
 from pyspark.sql import types as st
@@ -7,25 +7,21 @@ from pyspark.sql.functions import create_map, lit
 from pyspark.sql.types import StructType, StructField, StringType
 import sys 
 from elasticsearch import Elasticsearch
-
+from sentence_transformers import SentenceTransformer
 import hashlib
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from elasticsearch.helpers import bulk
+from langchain_elasticsearch import ElasticsearchStore
+import elasticsearch
+from langchain_nomic import NomicEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from elasticsearch import Elasticsearch
+
 def generate_sha256_hash_from_text(text) -> str:
     sha256_hash = hashlib.sha256()
     sha256_hash.update(text.encode('utf-8'))
     return sha256_hash.hexdigest()
 
-spark = SparkSession.builder.appName("kafkatospark").getOrCreate()
-spark.sparkContext.setLogLevel("ERROR")
-kafkaServer="broker:9092"
-topic = "datapipe"
-es_host = "http://elasticsearch:9200"
-es_index = "spark-index"
-es = Elasticsearch(
-    es_host,
-)
-#from sentence_transformers import SentenceTransformer
-
-#model = SentenceTransformer("nomic-ai/nomic-embed-text-v1", trust_remote_code=True)
 
 class CustomEmbeddingFunction:
     def __init__(self,model ):
@@ -44,9 +40,63 @@ class CustomEmbeddingFunction:
         else:
             return []
 
-# Creazione dell'istanza della classe di funzione di embedding
+def process_batch(batch_df, batch_id):
+    for row in batch_df.collect():
+        document = row['content'] 
+        if document is None:
+            print("non cè ninete")
+            return
+        splits = text_splitter.split_text(document)
+        
+        for split in splits:
+            hash = generate_sha256_hash_from_text(split)
+            # Controlla l'esistenza di ciascun hash/documento
+            if not es.exists(index=es_index, id=hash):
+                try:
+                    ids = [hash]
+                    texts = [split]
+                    resp = es.add_texts(index=es_index, ids=ids,texts=texts )
+                    print(resp)
+                except Exception as e:
+                    print(f"\rErrore nell'invio del documento a Elastic: {e}",end = "")
+
+            else: 
+                print("gia presente")
+
+
+
+#model = SentenceTransformer("nomic-ai/nomic-embed-text-v1", trust_remote_code=True)
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
 #embedding_function = CustomEmbeddingFunction(model)
 
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+spark = SparkSession.builder.appName("kafkatospark").getOrCreate()
+
+kafkaServer="broker:9092"
+topic = "datapipe"
+            
+
+es_index = "spark-index"
+embedding_function = CustomEmbeddingFunction(model)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+#es_host = Elasticsearch(es_host="http://elasticsearch:9200",index_name= es_index)
+
+es = ElasticsearchStore(
+    index_name= es_index,
+    es_url="http://localhost:9200",
+    embedding= HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2", model_kwargs={"device": "cpu"}
+    ),
+)
+
+
+
+
+# Creazione dell'istanza della classe di funzione di embedding
 
 article_schema = StructType([
         StructField("url", StringType(), True),
@@ -80,23 +130,6 @@ df = spark.readStream.format('kafka') \
         .selectExpr("data.articles.content")
 #.select(from_json(df.json, schema).alias('rowdata')) \
         
-def process_batch(batch_df, batch_id):
-    for row in batch_df.collect():
-        document = row['content'] 
-        if document is None:
-            print("non cè ninete")
-            return
-        hash = generate_sha256_hash_from_text(document)
-        esiste = es.exists(index=es_index, id=hash)
-        if esiste == True:
-            return
-        
-        try:
-            
-            resp = es.index(index=es_index, id=hash, body={"content":document})
-            print(resp)
-        except Exception as e:
-            print(f"\rErrore nell'invio del documento a Elastic: {e}",end = "")
 
 
 
